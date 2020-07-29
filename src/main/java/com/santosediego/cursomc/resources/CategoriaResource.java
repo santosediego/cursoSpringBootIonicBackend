b20e5b9fc1dd7eec8564a5de59f854eaad594dff
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-07-29 09:30:22 -0300

    Criando a classe Categoria e seu teste

--- src/main/java/com/santosediego/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/santosediego/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.santosediego.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.santosediego.cursomc.domain.Categoria;
 
 @RestController //Anotações necessárias para Rest;
 @RequestMapping(value = "/categorias")//Necessário para direcionar a url para esse recurso;
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)//Para a função poder funcionar deve-se anota-la com o devido metodo;
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {//mudar o método para lista;
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }

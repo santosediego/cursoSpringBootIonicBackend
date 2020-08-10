7710d5fe244fc2c69157322559353fa77fb479d5
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-08-10 11:07:37 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/santosediego/cursomc/CursomcApplication.java
+++ src/main/java/com/santosediego/cursomc/CursomcApplication.java
@@ -72,6 +72,14 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama, mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+		Categoria cat8 = new Categoria(null, "Eletrodomesticos");
+		Categoria cat9 = new Categoria(null, "Moveis");
+		Categoria cat10 = new Categoria(null, "Pet");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -85,7 +93,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p3.getCategorias().add(cat1);
 		
 		/*saveAll a partir do Spring 2.(...)*/
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/santosediego/cursomc/services/CategoriaService.java
+++ src/main/java/com/santosediego/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.santosediego.cursomc.domain.Categoria;
@@ -12,47 +15,52 @@ import com.santosediego.cursomc.repositories.CategoriaRepository;
 import com.santosediego.cursomc.services.exceptions.DataIntegrityException;
 import com.santosediego.cursomc.services.exceptions.ObjectNotFoundException;
 
-
 @Service
 public class CategoriaService {
 
-	@Autowired //A depedência será auto instanciada pelo Spring Boot com esta anotação;
+	@Autowired // A depedência será auto instanciada pelo Spring Boot com esta anotação;
 	private CategoriaRepository repo;// A interface.
-	
+
 	public Categoria find(Integer id) {
-		
+
 		Optional<Categoria> obj = repo.findById(id);
-		
-		/*Esse modelo é para o Sprint a partir da 2.0*/
+
+		/* Esse modelo é para o Sprint a partir da 2.0 */
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
-		
+
 		obj.setId(null);
-		
+
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
-		
+
 		find(obj.getId());
-		
+
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
-			repo.deleteById(id);			
-		}catch (DataIntegrityViolationException e) {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
 		}
 	}
-	
-public List<Categoria> findAll() {
-		
+
+	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);
 	}
 }

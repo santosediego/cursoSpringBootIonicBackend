7cd4047603be8581c927ffd6c258b5921eb82bca
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-08-12 09:58:50 -0300

    PUT, GET, DELETE para Cliente

--- src/main/java/com/santosediego/cursomc/services/ClienteService.java
+++ src/main/java/com/santosediego/cursomc/services/ClienteService.java
@@ -1,27 +1,69 @@
 package com.santosediego.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.santosediego.cursomc.domain.Cliente;
+import com.santosediego.cursomc.dto.ClienteDTO;
 import com.santosediego.cursomc.repositories.ClienteRepository;
+import com.santosediego.cursomc.services.exceptions.DataIntegrityException;
 import com.santosediego.cursomc.services.exceptions.ObjectNotFoundException;
-
 
 @Service
 public class ClienteService {
 
-	@Autowired //A depedência será auto instanciada pelo Spring Boot com esta anotação;
+	@Autowired // A depedência será auto instanciada pelo Spring Boot com esta anotação;
 	private ClienteRepository repo;// A interface.
-	
+
 	public Cliente find(Integer id) {
-		
+
 		Optional<Cliente> obj = repo.findById(id);
-		
-		/*Esse modelo é para o Sprint a partir da 2.0*/
+
+		/* Esse modelo é para o Sprint a partir da 2.0 */
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	public Cliente update(Cliente obj) {
+
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir o cliente porque possui entidades relacionadas.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }

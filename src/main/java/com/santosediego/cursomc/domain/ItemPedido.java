302fa508d129a83b933389e42ec6fca78038ca34
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-08-20 10:54:56 -0300

    Subtotal e total do pedido

--- src/main/java/com/santosediego/cursomc/domain/ItemPedido.java
+++ src/main/java/com/santosediego/cursomc/domain/ItemPedido.java
@@ -34,6 +34,10 @@ public class ItemPedido implements Serializable{
 		this.preco = preco;
 	}
 	
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
 	//Para melhorar a semantica da classe;
 	@JsonIgnore
 	public Pedido getPedido() {

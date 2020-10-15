40637726c102869cdb9a803072cff0f0809a7d41
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-10-15 06:42:25 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/santosediego/cursomc/CursomcApplication.java
+++ src/main/java/com/santosediego/cursomc/CursomcApplication.java
@@ -1,12 +1,18 @@
 package com.santosediego.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.santosediego.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	/* Implemenar a CommandLineRunner para puxar a função run abaixo */
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,6 +20,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\Users\\Diego Santos\\OneDrive\\Pictures\\estrelasMyPhoto.jpg");
 	}
 }

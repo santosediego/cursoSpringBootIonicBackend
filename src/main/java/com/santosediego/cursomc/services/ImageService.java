91d64847336e062c243eb2491f11f6ea68575e70
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2023-01-12 06:29:35 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/santosediego/cursomc/services/ImageService.java
+++ src/main/java/com/santosediego/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -52,5 +53,23 @@ public class ImageService {
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+
+		// Descobre qual posição é menor, largura ou altura;
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+
+		return Scalr.crop( // Fazer o recorte na imagem
+				sourceImg,
+				(sourceImg.getWidth() / 2) - (min / 2),
+				(sourceImg.getHeight() / 2) - (min / 2),
+				min,
+				min);
+	}
+
+	// Redimensionar a imagem
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }
